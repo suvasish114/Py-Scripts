57c2d01ff4a9ba0b1ad32d727cdbb87d21f4c0e8
suvasish114 <misuvasish114@gmail.com>
2020-11-01 11:24:56 +0530

    auto installing packages

--- barcode.py
+++ barcode.py
@@ -15,29 +15,43 @@ UniversalProductCodeA
 '''
 # imported modules
 import sys
-import barcode
+import pip
+try:
+    import barcode
+except:
+    print('this script need some additional packages:')
+    choice = input('do you want to install them ?(y/n) ')
+    # installing additional packages
+    if choice == 'y' or choice == 'Y' :
+        print('installing...')
+        pip.main(['install','python-barcode'])
+        pip.main(['install','pillow'])
+    else:
+        sys.exit()
 
-# print the top logo
-def ascii_Art():
-    print(" ║█║▌║█║▌│║▌█║▌║ \nBARCODE-GENERETOR\n")
+finally:
+    # print the top logo
+    def ascii_Art():
+        print(" ║█║▌║█║▌│║▌█║▌║ \nBARCODE-GENERETOR\n")
 
-# barcode class
-class barcode_generator:
-    def __init__(self, user_code):
-        self.code = user_code
-    def generate(self):
-        self.ean = barcode.get('ean13', self.code)
-        print('GENERATE SUCESSFULLY')
-        self.filename = self.ean.save('barcode')
+    # barcode class
+    class barcode_generator:
+        def __init__(self, user_code):
+            self.code = user_code
+        def generate(self):
+            self.ean = barcode.get('ean13', self.code)
+            print('+----------------------+'+'\n| GENERATE SUCESSFULLY |'+'\n+----------------------+')
+            self.filename = self.ean.save('barcode')
 
-ascii_Art()
+    ascii_Art()
 
-# if run as a script
-if __name__ == '__main__':
-    bcd_code = input('ENTER THE CODE: ')
-    try:
-        bcd_obj = barcode_generator(bcd_code)
-        bcd_obj.generate()
-    except:
-        print("ERROR")
-    pass
+    # if run as a script
+    if __name__ == '__main__':
+        bcd_code = input('ENTER THE CODE: ')
+        try:
+            bcd_obj = barcode_generator(bcd_code)
+            bcd_obj.generate()
+        except:
+            print("ERROR")
+        else:
+    	    sys.exit()
